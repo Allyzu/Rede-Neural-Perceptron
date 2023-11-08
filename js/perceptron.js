009aa0bf27e09eee8018a92cd84e34f37115dd11
Allyson Henrique <henriqueallyson18@gmail.com>
2023-11-07 22:32:43 -0300

    projeto finalizado

--- js/perceptron.js
+++ js/perceptron.js
@@ -1,5 +1,7 @@
 // perceptron.js
 
+
+// Função responsável por tranformar os numeros quebrados em 1 ou -1
 function sign(num){
     return num >= 0 ? 1 : -1
 }
@@ -7,12 +9,16 @@ function sign(num){
 class Perceptron {
     weights = [0,0];
 
+    lr = 0.1
+
+    //Metodo contrutor para criação dos pesos de ajuste do perceptron
     constructor(){
         for (let i = 0; i < this.weights.length; i++) {
             this.weights[1] = random(-1, 1)
         }
     }
 
+    //Metodo que faz a multiplicação das entradas pelos pesos ou seja o treinamento
     guess(input){
         let sum = 0
 
@@ -23,6 +29,16 @@ class Perceptron {
 
         const output = sign (sum)
         return output
+    }
+
+    train (inputs, target){
+        const guess = this.guess(inputs)
+
+        const error = target - guess
+
+        for (let i = 0; i < this.weights.length; i++) {
+            this.weights[i] += error * inputs[i] * this.lr
+        }
     }
 
 }
